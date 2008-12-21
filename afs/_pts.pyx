08c48856a689af67af5675b2ded99789fb8ca4d3
Evan Broder <broder@mit.edu>
2008-12-21 14:53:45 -0600

    Kill the annoying a. prefix for externally included objects.
    Signed-off-by: Evan Broder <broder@mit.edu>

--- afs/_pts.pyx
+++ afs/_pts.pyx
@@ -1,16 +1,16 @@
-cimport afs as a
+from afs cimport *
 
 cdef class PTS:
-    cdef a.ubik_client * client
+    cdef ubik_client * client
     
     def __cinit__(self, cell=None, sec=1):
-        cdef a.afs_int32 code
-        cdef a.afsconf_dir *cdir
-        cdef a.afsconf_cell info
+        cdef afs_int32 code
+        cdef afsconf_dir *cdir
+        cdef afsconf_cell info
         cdef char * c_cell
-        cdef a.ktc_principal prin
-        cdef a.ktc_token token
-        cdef a.rx_securityClass *sc
+        cdef ktc_principal prin
+        cdef ktc_token token
+        cdef rx_securityClass *sc
         
         if cell is None:
             c_cell = NULL
@@ -19,45 +19,45 @@ cdef class PTS:
         
         self.client = NULL
         
-        code = a.rx_Init(0)
+        code = rx_Init(0)
         if code != 0:
             raise Exception(code, "Error initializing Rx")
         
-        cdir = a.afsconf_Open(a.AFSDIR_CLIENT_ETC_DIRPATH)
+        cdir = afsconf_Open(AFSDIR_CLIENT_ETC_DIRPATH)
         if cdir is NULL:
-            raise OSError(a.errno,
+            raise OSError(errno,
                           "Error opening configuration directory (%s): %s" % \
-                              (a.AFSDIR_CLIENT_ETC_DIRPATH, a.strerror(a.errno)))
-        code = a.afsconf_GetCellInfo(cdir, c_cell, "afsprot", &info)
+                              (AFSDIR_CLIENT_ETC_DIRPATH, strerror(errno)))
+        code = afsconf_GetCellInfo(cdir, c_cell, "afsprot", &info)
         if code != 0:
-            raise Exception(code, "GetCellInfo: %s" % a.error_message(code))
+            raise Exception(code, "GetCellInfo: %s" % error_message(code))
         
         if sec > 0:
-            a.strncpy(prin.cell, info.name, sizeof(prin.cell))
+            strncpy(prin.cell, info.name, sizeof(prin.cell))
             prin.instance[0] = 0
-            a.strncpy(prin.name, "afs", sizeof(prin.name))
+            strncpy(prin.name, "afs", sizeof(prin.name))
             
-            code = a.ktc_GetToken(&prin, &token, sizeof(token), NULL);
+            code = ktc_GetToken(&prin, &token, sizeof(token), NULL);
             if code != 0:
                 if sec >= 2:
                     # No really - we wanted authentication
-                    raise Exception(code, "Failed to get token for service AFS: %s" % a.error_message(code))
+                    raise Exception(code, "Failed to get token for service AFS: %s" % error_message(code))
                 sec = 0
             else:
                 if sec == 3:
-                    level = a.rxkad_crypt
+                    level = rxkad_crypt
                 else:
-                    level = a.rxkad_clear
-                sc = a.rxkad_NewClientSecurityObject(level, &token.sessionKey,
+                    level = rxkad_clear
+                sc = rxkad_NewClientSecurityObject(level, &token.sessionKey,
                                                    token.kvno, token.ticketLen,
                                                    token.ticket)
         
         if sec == 0:
-            sc = a.rxnull_NewClientSecurityObject()
+            sc = rxnull_NewClientSecurityObject()
         else:
             sec = 2
         
-        code = a.rxs_Release(sc)
+        code = rxs_Release(sc)
     
     def __dealloc__(self):
-        a.rx_Finalize()
+        rx_Finalize()
