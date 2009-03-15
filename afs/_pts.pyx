a7a1e67a14a9c997fd668ca874687dffccb92491
Evan Broder <broder@mit.edu>
2009-03-15 18:49:44 -0400

    Remove the reserved field from prcheckentry, since it doesn't seem to be used by anything.
    Signed-off-by: Evan Broder <broder@mit.edu>

--- afs/_pts.pyx
+++ afs/_pts.pyx
@@ -27,7 +27,6 @@ cdef import from "afs/ptuser.h":
         afs_int32 creator
         afs_int32 ngroups
         afs_int32 count
-        afs_int32 reserved[5]
         char name[PR_MAXNAMELEN]
 
     int ubik_PR_NameToID(ubik_client *, afs_int32, namelist *, idlist *)
@@ -55,7 +54,6 @@ cdef class PTEntry:
     cdef public afs_int32 creator
     cdef public afs_int32 ngroups
     cdef public afs_int32 count
-    cdef afs_int32 reserved[5]
     cdef public char * name
 
 cdef int _ptentry_from_c(PTEntry p_entry, prcheckentry c_entry) except -1:
@@ -69,7 +67,6 @@ cdef int _ptentry_from_c(PTEntry p_entry, prcheckentry c_entry) except -1:
     p_entry.creator = c_entry.creator
     p_entry.ngroups = c_entry.ngroups
     p_entry.count = c_entry.count
-    memcpy(p_entry.reserved, c_entry.reserved, sizeof(p_entry.reserved))
     p_entry.name = c_entry.name
     return 0
 
@@ -84,7 +81,6 @@ cdef int _ptentry_to_c(prcheckentry * c_entry, PTEntry p_entry) except -1:
     c_entry.creator = p_entry.creator
     c_entry.ngroups = p_entry.ngroups
     c_entry.count = p_entry.count
-    memcpy(c_entry.reserved, p_entry.reserved, sizeof(p_entry.reserved))
     strncpy(c_entry.name, p_entry.name, sizeof(c_entry.name))
     return 0
 
