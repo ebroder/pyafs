69ce5cfed0b0efa0d573f1bcdbfad68fa7b2fce3
Evan Broder <broder@mit.edu>
2009-11-19 02:55:34 -0500

    Add an fs.inafs function for determining if a path is in AFS at all.
    Signed-off-by: Evan Broder <broder@mit.edu>

--- afs/fs.py
+++ afs/fs.py
@@ -1,2 +1,13 @@
+import errno
 import _fs
 from _fs import whichcell
+
+def inafs(path):
+    """Return True if a path is in AFS."""
+    try:
+        whichcell(path)
+    except OSError, e:
+        if e.errno in (errno.EINVAL, errno.ENOENT):
+            return False
+
+    return True
