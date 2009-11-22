ebroder/pyafs

commit 0e2974f1af7500997666a21880af9a88947a8125

Always use absolute imports.
Signed-off-by: Evan Broder <broder@mit.edu>

afs/acl.py
@@ -1,7 +1,7 @@
-import _acl
-from _acl import READ, WRITE, INSERT, LOOKUP, DELETE, LOCK, ADMINISTER, \
+import afs._acl
+from afs._acl import READ, WRITE, INSERT, LOOKUP, DELETE, LOCK, ADMINISTER, \
     USR0, USR1, USR2, USR3, USR4, USR5, USR6, USR7
-from _acl import getCallerAccess
+from afs._acl import getCallerAccess
 
 _canonical = {
     "read": "rl",

afs/fs.py
@@ -1,6 +1,6 @@
 import errno
-import _fs
-from _fs import whichcell
+import afs._fs
+from afs._fs import whichcell
 
 def inafs(path):
     """Return True if a path is in AFS."""

afs/pts.py
@@ -1,5 +1,5 @@
 import collections
-import _pts
+from afs import _pts
 
 class PTRelationSet(collections.MutableSet):
     """Collection class for the groups/members of a PTEntry.

commit d1b2fea3082cbc0651443dab1c0f5a03f11be435

Fix typos in afs.acl and afs.fs introduced in 0e2974f1.

afs/acl.py
@@ -1,4 +1,4 @@
-import afs._acl
+from afs import _acl
 from afs._acl import READ, WRITE, INSERT, LOOKUP, DELETE, LOCK, ADMINISTER, \
     USR0, USR1, USR2, USR3, USR4, USR5, USR6, USR7
 from afs._acl import getCallerAccess

afs/fs.py
@@ -1,5 +1,5 @@
 import errno
-import afs._fs
+from afs import _fs
 from afs._fs import whichcell
 
 def inafs(path):